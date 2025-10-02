ba468391065ca9b3df7d02f33a03ca6f2e120d18
Maria Clara Pereira de Souza <160674059+mariapcsouza@users.noreply.github.com>
2025-10-02 20:50:29 -0300

    html do login até a compra

---
+++ LoginGgmax.py
@@ -0,0 +1,29 @@
+# clicar no menu:
+<i class="icon-menu-03"></i>
+
+# entrar, clicando no botao do menu suspenso:
+
+<div class="account__popover">
+<ul class="account-menu">
+<li>
+<a href="javascript:void(0)"> Entrar </a>...
+
+# email
+
+<input class="input-field" type="text" placeholder="Usuário ou e-mail">
+
+"saulocardoso3557314@gmail.com"
+
+# senha
+
+<input name="password" class="input-field" type="password" placeholder="Senha">
+
+"Be2025ta$"
+
+# entrar
+
+<button class="btn btn-primary">Entrar</button>
+
+# escolher item
+
+<span class="button__value">Escolha um item</span>
