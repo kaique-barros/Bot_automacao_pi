a6c0be49444942688c811f61e2c4c27723bc33b1
kaique-barros <kaiqbarss@outlook.com>
2025-10-08 15:18:18 -0300

    Remove __pycache__ from repository

--- main.py
+++ main.py
@@ -4,21 +4,27 @@ from seleniumbase import Driver
 from src.proxy import Proxy
 import threading
 import ngrok
+from dotenv import load_dotenv
+import os
 
-# --- CHAVES DE API ---
-# Passar para um arquivo .env posteriormente
-CAPSOLVER_API_KEY = "CAP-D38F9278056E5453E567FB2115F9D2FF89C61B09C5F64FDBE74D1B76D2A81521"
-NGROK_KEY = '33cXA5fJv78EF8LLikmVuezl0rE_7Zi16aFZhszhdxG4zwbx2'
+load_dotenv(override=True)
+
+# -----------------------------------------------------------------------------------------------------
+# ------------------- CONFIGURAÇÔES -------------------------------------------------------------------
+# --- Chaves de API
+CAPSOLVER_API_KEY = 'CAP-ED5DF302811B699CA76FC80712D56EF5044F6B693FFF7E5E53DCD377F7CF28B0'
+NGROK_KEY = '33nepndDespFrKvWB8LueTLZVbj_7pMXHYADta3Vy2Q5JHeTM'
 
 # --- Configurações do scrapper
 GGMAX_SITE_KEY = "0x4AAAAAAADnPIDROrmt1Wwj" 
-PAGE_URL = "https://ggmax.com.br"
+PAGE_URL = "https://ggmax.com.br/anuncio/venda-de-brinrots-raros-no-roblox-entrega-rapida-e-segura"
 
 # --- Configurações do proxy
-PROXY_USER = 'usuario'
-PROXY_PASS = 'senha'
+PROXY_USER = '98d5e8f4b835e0968ce439df645264ba3dbea6d17f374a5afe83846be09fdb4c'
+PROXY_PASS = 'b4604382119649c7f6fdb81bb4071204ccebded8a31cc0f555605d5ce14a8bed'
 PROXY_IP = '127.0.0.1'
 PROXY_PORT = 5001
+# -----------------------------------------------------------------------------------------------------
 
 # Inicia o proxy
 proxy = Proxy(PROXY_USER, PROXY_PASS, PROXY_IP, PROXY_PORT)
@@ -39,7 +45,7 @@ try:
   capsolver.api_key = CAPSOLVER_API_KEY
   solution = capsolver.solve({
     "type": "AntiCloudflareTask",
-    "websiteURL": "https://ggmax.com.br",
+    "websiteURL": PAGE_URL,
     "proxy": PROXY_CONNECTION
   })
   print(f'Solução do captcha recebida: {solution['token'][:10]}')
@@ -57,7 +63,7 @@ try:
       }
       driver.add_cookie(cookie)
       print(f"Cookie {cookie_name} adicionado.")
-      
+
     driver.refresh()
     
     time.sleep(20)
