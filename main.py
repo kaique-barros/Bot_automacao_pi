7f1bd17d96dd0ec32977b5c1d59b0e4702f51342
kaique-barros <kaiqbarss@outlook.com>
2025-10-13 17:45:56 -0300

    Organização dos diretórios, integração do processo de compra e login à main e correção de bugs

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import time
-import capsolver
-from seleniumbase import Driver
-from src.proxy import Proxy
-import threading
 import ngrok
+import requests
+import threading
+import capsolver
+from src.proxy import Proxy
+from src.buyProcess import BuyProccesss
+from src.Login import Login_process
+from seleniumbase import Driver
 
 # -----------------------------------------------------------------------------------------------------
 # ------------------- CONFIGURAÇÔES -------------------------------------------------------------------
 # --- Chaves de API
 CAPSOLVER_API_KEY = 'CAP-ED5DF302811B699CA76FC80712D56EF5044F6B693FFF7E5E53DCD377F7CF28B0'
-NGROK_KEY =         '33nepndDespFrKvWB8LueTLZVbj_7pMXHYADta3Vy2Q5JHeTM'
+NGROK_KEY =         '33yZX8AEwoIrDQToGVY7lO4YXlS_2L2Q7Q9oFBC91DDb5mAMb'
 
 # --- Configurações do scrapper
 GGMAX_SITE_KEY =    '0x4AAAAAAADnPIDROrmt1Wwj' 
@@ -20,37 +23,54 @@ PROXY_USER =        '98d5e8f4b835e0968ce439df645264ba3dbea6d17f374a5afe83846be09
 PROXY_PASS =        'b4604382119649c7f6fdb81bb4071204ccebded8a31cc0f555605d5ce14a8bed'
 PROXY_IP =          '127.0.0.1'
 PROXY_PORT =        5001
+
+# --- Configurações do login
+USUARIO = "kaique.barros@pijunior.com.br"
+SENHA = "Abc12345!"
+login_process = Login_process(USUARIO, SENHA)
+
+# --- Configurações do processo de compra
+NOME = "Marcos Camargo Oliveira Bakker da Silva"
+WAIT = 10
+PRICE = "R$ " + "2,00"
+buy_process = BuyProccesss(NOME, WAIT, PRICE)
+NUM_PURCHASES = 1
+
+# --- Configurações do EfiBank 
+PIX_KEY = ""
+INFO = "Pagamento de QR Code via API Pix"
 # -----------------------------------------------------------------------------------------------------
 
 # Inicia o proxy
 proxy = Proxy(PROXY_USER, PROXY_PASS, PROXY_IP, PROXY_PORT)
 proxy_thread = threading.Thread(target=proxy.start_proxy, daemon=True)
 proxy_thread.start()
-print("Proxy iniciado.")
+print("[*] Proxy iniciado.")
 
 try: 
   # Abre o tunel no ngrok
   ngrok.set_auth_token(NGROK_KEY)
   listener = ngrok.forward(PROXY_PORT, 'tcp')
   ngrok_port = listener.url().split(":")[2]
+  print("[*] Tunel ngrok aberto.")
 
   PROXY_CONNECTION = f'socks5://{PROXY_USER}:{PROXY_PASS}@0.tcp.sa.ngrok.io:{ngrok_port}'
 
   # Solicita a resolução o captcha
-  print("Solicitando resolução do captcha.")
+  print("[*] Solicitando resolução do captcha.")
   capsolver.api_key = CAPSOLVER_API_KEY
   solution = capsolver.solve({
     "type": "AntiCloudflareTask",
     "websiteURL": PAGE_URL,
     "proxy": PROXY_CONNECTION
   })
-  print(f'Solução do captcha recebida: {solution['token'][:10]}')
+  print(f'[*] Solução do captcha recebida: {solution['token'][:10]}')
 
-  with Driver(browser='chrome', uc=True, agent=solution['userAgent'], headless=False) as driver:
+  with Driver(browser='chrome', uc=True, agent=solution['userAgent'], headless=False, port=9222) as driver:
     driver.open(PAGE_URL)
-    print("Pagina aberta")
+    print("[*] Pagina aberta")
     
-    print("Setando os cookies")
+    print("[*] Setando os cookies")
     for cookie_name in solution['cookies']:
       cookie = {
         'name': cookie_name,
@@ -58,10 +78,22 @@ try:
         'domain': '.ggmax.com.br'
       }
       driver.add_cookie(cookie)
-      print(f"Cookie {cookie_name} adicionado.")
+      print(f"[*] Cookie {cookie_name} adicionado.")
 
+    print("[*] Recarregando a página.")
     driver.refresh()
     
-    time.sleep(20)
+    print("[*] Iniciando o processo de login.")
+    login_process.login()
+    
+    time.sleep(30)
+    
+    # print("[*] Iniciando o processo de compra.")
+    # for i in range(NUM_PURCHASES):
+    #   print(f"[*] Iniciando a compra {i+1} de {NUM_PURCHASES}.")
+    #   buy_process.buy()
+    #   driver.open(PAGE_URL)
+    
+    
 except KeyboardInterrupt:
   print("Execução encerrada.")
---
+++ src/Login.py
@@ -0,0 +1,59 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+WAIT = 10
+
+class Login_process:
+    def __init__(self, usuario, senha):
+        self.USUARIO = usuario
+        self.SENHA = senha
+    
+    def login(self):
+        """Acessa site GGmax já aberto no Chrome"""
+        options = webdriver.ChromeOptions()
+        options.debugger_address = "127.0.0.1:9222"  # porta padrao do --remote-debugging
+        driver = webdriver.Chrome(options=options)
+        wait = WebDriverWait(driver, WAIT)
+
+        try:
+            menu_icon = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".icon-menu-03")))
+            menu_icon.click()
+            print("Menu aberto.")
+        except Exception as e:
+                print("Erro ao abrir menu:", e)
+            
+        try:
+            entrar_link = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='account__popover']//a[contains(text(), 'Entrar')]")))
+            entrar_link.click()
+            print("Cliquei no botão 'Entrar'.")
+        except Exception as e:
+                print("Erro ao clicar no botão de entrar:", e)
+            
+        try:
+            campo_email = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[placeholder='Usuário ou e-mail']")))
+            campo_email.send_keys(self.USUARIO)
+            print("E-mail preenchido.")
+            campo_senha = driver.find_element(By.CSS_SELECTOR, "[name='password']")
+            campo_senha.send_keys(self.SENHA)
+            print("Senha preenchida.")
+        except Exception as e:
+                print("Erro ao preencher dados do usuário:", e)
+            
+        try:
+            botao_entrar_login = wait.until(EC.element_to_be_clickable((By.XPATH, "//form[contains(@class, 'ggmax-login-form')]//button[contains(text(), 'Entrar')]")))
+            botao_entrar_login.click()
+            print("Cliquei no botão final de login.")
+        except Exception as e:
+                print("Erro ao clicar no botão de finalizar login:", e)
+
+        # try:
+        #     wait.until(EC.presence_of_element_located(()))
+        #     print("Login realizado com sucesso!")
+        # except Exception as e:
+        #     print(f"Ocorreu um erro durante o processo de login: {e}")
+
+        finally:
+            pass
